913611e3047a4cee4fd82f04a628477b58108a0e
Ashok Mane <ashok@example.com>
2023-03-18 16:06:36 +0530

    added sysout to print bean count and names

--- src/main/java/org/ashok/springboot/SimpleSpringbootWebappMonolithApplication.java
+++ src/main/java/org/ashok/springboot/SimpleSpringbootWebappMonolithApplication.java
@@ -2,12 +2,19 @@ package org.ashok.springboot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
 
 @SpringBootApplication
 public class SimpleSpringbootWebappMonolithApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SimpleSpringbootWebappMonolithApplication.class, args);
+		ApplicationContext appContext = SpringApplication.run(SimpleSpringbootWebappMonolithApplication.class, args);
+		
+		System.out.println("BeansCount:" + appContext.getBeanDefinitionCount());
+		for(String beanName: appContext.getBeanDefinitionNames())
+		{
+			System.out.println(beanName);
+		}
 	}
 
 }
